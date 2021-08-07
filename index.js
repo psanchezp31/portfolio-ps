b8ca3b39bd9519faa08f00fd34b81e0be453b4a4
Paula Sánchez P <paula.sanchez1031@gmail.com>
2021-08-07 08:47:25 -0500

    Slider view-bug fixed

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-import Glide from '@glidejs/glide'
+const {Breakpoints} = require("@glidejs/glide/dist/glide.modular.esm");
 
 document.body.addEventListener("click", function (e) {
-  if (e.target.matches("css-doodle")) {
-    e.target.refresh();
-  }
+    if (e.target.matches("css-doodle")) {
+        e.target.refresh();
+    }
 });
 
 let grid = document.getElementById("technologies-grid");
 
 let icons = {
-  JavaScript: "icon-javascript-alt",
-  Html5: "icon-html5-alt",
-  Css3: "icon-css3-alt",
-  React: "icon-reactjs",
-  VueJs:"fab fa-vuejs",
-  Python: "icon-python",
-  Java: " icon-java",
-  SpringBoot: "icon-spring",
-  Git: "icon-git",
-  MySQL: "icon-mysql",
+    JavaScript: "icon-javascript-alt",
+    Html5: "icon-html5-alt",
+    Css3: "icon-css3-alt",
+    React: "icon-reactjs",
+    VueJs: "fab fa-vuejs",
+    Python: "icon-python",
+    Java: " icon-java",
+    SpringBoot: "icon-spring",
+    Git: "icon-git",
+    MySQL: "icon-mysql",
 };
 
 let fragment = document.createDocumentFragment();
 
-
 Object.keys(icons).forEach(function (icon) {
-  let i = document.createElement("i");
-  i.id = icon;
-  i.className = icons[icon] + " overlay";
-  if(i.id == "VueJs"){
-    i.className += " iconfa";
-  }
-  i.addEventListener("mouseover", mouseOver, false);
-  i.addEventListener("mouseout", mouseOut, false);
-  fragment.appendChild(i);
+    console.log(icon)
+    let i = document.createElement("i");
+    i.id = icon;
+    i.className = icons[icon] + " overlay";
+    if (i.id == "VueJs") {
+        i.className += " iconfa";
+    }
+    i.addEventListener("mouseover", mouseOver, false);
+    i.addEventListener("mouseout", mouseOut, false);
+    fragment.appendChild(i);
 });
 
 grid.appendChild(fragment);
@@ -42,43 +42,49 @@ grid.children;
 //console.log(grid)
 
 function mouseOver(event) {
-  let elementHoverId = this.id;
-  event.target.innerHTML = elementHoverId;
-  if(elementHoverId == 'VueJs'){
+    let elementHoverId = this.id;
+    event.target.innerHTML = elementHoverId;
+    if (elementHoverId == 'VueJs') {
+        event.target.innerHTML = "";
+    }
+    event.target.style.fontSize = "15px";
+    event.target.style.color = "#4d089a";
+    event.target.style.fontStyle = "normal";
+}
+
+function mouseOut(event) {
     event.target.innerHTML = "";
-  }
-  event.target.style.fontSize = "15px";
-  event.target.style.color="#4d089a";
-  event.target.style.fontStyle = "normal";
+    event.target.style.fontSize = "50px";
+    event.target.style.transition = ".3s ease";
 }
 
-function mouseOut(event){
-  event.target.innerHTML = "";
-  event.target.style.fontSize = "50px";
-  event.target.style.transition =".3s ease";
-}
-
+/*Projects slider */
 
 window.addEventListener('load', () => {
-    
+
     const glide = new Glide('.glide', {
-       type: "carousel",
+        type: "carousel",
         touchAngle: 45,
         focusAt: 1,
-         startAt: 1,
-         perView: 3.5,
-         keyboard: true,
-         gap: 20,
-         autoplay: false,
-      hoverpause: true,
-      animationDuration:5000,
-         peek: {
-         before: 50,
-         after: 50
-      },
-      
+        startAt: 1,
+        perView: 3.5,
+        keyboard: true,
+        gap: 20,
+        autoplay: false,
+        hoverpause: true,
+        animationDuration: 5000,
+        peek: {
+            before: 50,
+            after: 50
+        },
+
     })
-    
+    Breakpoints.match({
+        600: {perView: 1},
+        800: {perView: 2},
+        1200: {perView: 3}
+    })
+
     glide.mount()
 
 })
